6e7f121542ad42a738850d554cd45cebe6a2e31e
Itamar Held <itamar.held@pyramidanalytics.com>
2021-11-10 14:33:48 +0200

    todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { action, observable } from "mobx";
+import { observer } from "mobx-react";
+import { v4 as newUUID } from "uuid";
+import "./App.less";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface TODO {
+    id: string;
+    text: string;
+}
+
+@observer
+export class App extends React.Component {
+    @observable currentTodoText = "";
+    @observable todos: TODO[] = [];
+
+    render() {
+        return (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <div>
+                    <input
+                        value={this.currentTodoText}
+                        onChange={(e) => this.setTodoText(e.target.value)}
+                    ></input>
+                    <button
+                        disabled={!this.currentTodoText}
+                        onClick={this.addCurrentTodo}
+                    >
+                        +
+                    </button>
+                    <div className="todo-container">
+                        {this.todos.map((todo) => (
+                            <div style={{ display: "flex", padding: 5 }}>
+                                <div style={{ flex: 1 }} key={todo.id}>
+                                    {todo.text}
+                                </div>
+                                <button onClick={() => this.deleteTodo(todo)}>
+                                    x
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    @action
+    addCurrentTodo = () => {
+        const newTodo: TODO = {
+            id: newUUID(),
+            text: this.currentTodoText,
+        };
+
+        this.todos.push(newTodo);
+
+        this.setTodoText("");
+    };
+
+    @action
+    deleteTodo = (todoToDelete: TODO) => {
+        this.todos = this.todos.filter((todo) => todo.id !== todoToDelete.id);
+    };
+
+    @action
+    setTodoText(text: string) {
+        this.currentTodoText = text;
+    }
 }
 
 export default App;
